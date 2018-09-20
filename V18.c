886eb3b5a60ea08551470e40da90ddbe93489844
The-Joke-Breaker <mathias.steentoft@outlook.com>
2018-09-20 19:44:36 +0200

    change to sect 3

--- V18.c
+++ V18.c
@@ -401,7 +401,7 @@ void chooseSect() {
         resetMotorEncoder(motorR);
         resetMotorEncoder(motorL);
         resetMotorEncoder(motorG);
-    	setMotorTarget(motorG, grabdist, 100);
+    	moveMotorTarget(motorG, grabdist, 100);
     	waitUntilMotorStop(motorG);
 
        /* while (dist > 4) {
